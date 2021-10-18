5f8b242bd022fd911d154c357e514b47c355a88b
Jason Offenbaker <JasonOffenbaker@gmail.com>
2021-10-18 02:31:37 -0700

    when clicking on a previous search the results will load to the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,31 +8,39 @@ var responseUrl;
 var responseWeather;
 var cityLat;
 var cityLong;
-var searchHistoryEl = document.querySelector("citySearches");
 
-var updateSearchHistory = function (event) {
-    event.preventDefault();
+var updateSearchHistory = function (event, isHistory) {
+    if (isHistory === true) {
+        cityNameEl.textContent = newCitySearch + "(" + (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear() + ")";
+        getAPILatLongData();
+    } else {
+        event.preventDefault();
 
-    //creating elements
-    var newSearchHistory = document.createElement('div');
-    var newSearchValueEl = document.createElement('button');
-    newCitySearch = document.getElementById("searchForm").value;
+        //creating elements
+        var newSearchHistory = document.createElement('div');
+        var newSearchValueEl = document.createElement('button');
+        newCitySearch = document.getElementById("searchForm").value;
 
-    //classes
-    newSearchHistory.setAttribute("class", "citySearches")
-    newSearchValueEl.innerHTML = newCitySearch;
-    cityNameEl.innerHTML = newCitySearch + "(" + (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear() + ")";
+        //classes
+        newSearchHistory.setAttribute("class", "citySearches")
+        //newSearchValueEl.setAttribute("type", "submit")
+        newSearchValueEl.innerHTML = newCitySearch;
+        cityNameEl.innerHTML = newCitySearch + "(" + (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear() + ")";
 
 
 
-    //appends
-    searchListEl.prepend(newSearchHistory);
-    newSearchHistory.appendChild(newSearchValueEl);
+        //appends
+        searchListEl.prepend(newSearchHistory);
+        newSearchHistory.appendChild(newSearchValueEl);
 
-    //getAPILatLongData();
-    //updateCityWeather();
+        getAPILatLongData();
+
+        var searchHistoryEl = document.querySelector(".citySearches");
+        searchHistoryEl.addEventListener("click", searchFromHistory);
+
+        document.getElementById("searchForm").value = "";
+    }
 }
-
 
 function getAPILatLongData() {
     requestUrlLatLong = 'https://api.openweathermap.org/data/2.5/weather?q=' + newCitySearch + '&units=imperial' + '&appid=' + apiKey;
@@ -45,10 +53,8 @@ function getAPILatLongData() {
             cityLat = data.coord.lat;
             cityLong = data.coord.lon;
             responseURL = data;
-            console.log("Event 1")
         })
         .then(function (test) {
-            console.log("Event 2");
             getAPIWeather();
         });
 
@@ -63,7 +69,6 @@ function getAPIWeather() {
         })
         .then(function (data) {
             responseWeather = data;
-            console.log(data);
             updateCityWeather();
         });
 }
@@ -79,6 +84,11 @@ function updateCityWeather() {
     cityStatsWind.textContent = "Wind: " + responseURL.wind.speed + " MPH";
     cityStatsHumidity.textContent = "Humidity: " + responseURL.main.humidity + "%";
     cityStatsUV.textContent = "UV Index: " + responseWeather.current.uvi;
+
+}
+function searchFromHistory(event) {
+    newCitySearch = event.target.textContent;
+    updateSearchHistory(event.target.textContent, true);
 
 }
 
