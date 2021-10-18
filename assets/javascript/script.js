2dd75382dc9232521bf5498012761405167f5d1d
Jason Offenbaker <JasonOffenbaker@gmail.com>
2021-10-18 01:31:52 -0700

    added ability to get full weather data and display it to the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,13 +3,19 @@ var searchListEl = document.querySelector("#searchList")
 var cityNameEl = document.querySelector("#cityName")
 var newCitySearch;
 var currentDate = new Date();
+var apiKey = "9221d865d32d6411009be013bcda09f9";
+var responseUrl;
+var responseWeather;
+var cityLat;
+var cityLong;
+var searchHistoryEl = document.querySelector("citySearches");
 
 var updateSearchHistory = function (event) {
     event.preventDefault();
 
     //creating elements
     var newSearchHistory = document.createElement('div');
-    var newSearchValueEl = document.createElement('p');
+    var newSearchValueEl = document.createElement('button');
     newCitySearch = document.getElementById("searchForm").value;
 
     //classes
@@ -23,24 +29,56 @@ var updateSearchHistory = function (event) {
     searchListEl.prepend(newSearchHistory);
     newSearchHistory.appendChild(newSearchValueEl);
 
-    //getAPIData();
+    //getAPILatLongData();
+    //updateCityWeather();
 }
 
 
-function getAPIData() {
-    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + newCitySearch + '&units=imperial' + '&appid=9221d865d32d6411009be013bcda09f9'
-    fetch(requestURL)
+function getAPILatLongData() {
+    requestUrlLatLong = 'https://api.openweathermap.org/data/2.5/weather?q=' + newCitySearch + '&units=imperial' + '&appid=' + apiKey;
+
+    fetch(requestUrlLatLong)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.main.temp);
+            cityLat = data.coord.lat;
+            cityLong = data.coord.lon;
+            responseURL = data;
+            console.log("Event 1")
+        })
+        .then(function (test) {
+            console.log("Event 2");
+            getAPIWeather();
         });
 
-    updateCityWeather();
+}
+
+function getAPIWeather() {
+    requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLong + '&appid=' + apiKey;
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            responseWeather = data;
+            console.log(data);
+            updateCityWeather();
+        });
 }
 
 function updateCityWeather() {
+    var cityStatsTemp = document.querySelector("#temp");
+    var cityStatsWind = document.querySelector("#wind");
+    var cityStatsHumidity = document.querySelector("#humidity");
+    var cityStatsUV = document.querySelector("#uv");
+
+
+    cityStatsTemp.textContent = "Temp: " + responseURL.main.temp + "°F";
+    cityStatsWind.textContent = "Wind: " + responseURL.wind.speed + " MPH";
+    cityStatsHumidity.textContent = "Humidity: " + responseURL.main.humidity + "%";
+    cityStatsUV.textContent = "UV Index: " + responseWeather.current.uvi;
 
 }
 
